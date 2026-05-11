5d53658a94864d9ec59d50264a1099c1ff7f5a7f
NotFeez <zaharsilov70@gmail.com>
2026-05-11 23:54:16 +0300

    added main.cpp and cipher.cpp, no functions yet

---
+++ src/core/cipher.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <functional>
+
+using namespace std;
+
+//encrypting function
+void encrypt(const string& Input, const string& Key, const string& Output) {
+
+}
+
+//decrypting function
+void decrypt(const string& Input, const string& Key, const string& Output) {
+
+}
